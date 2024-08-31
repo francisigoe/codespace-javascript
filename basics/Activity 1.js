6bd4463453b3447c08caa818081e7c268b5ac893
francisigoe <frankisigoe@gmail.com>
2024-08-31 18:22:21 +0100

    added basics activities
    also changed file layout and made a file for testing

---
+++ basics/Activity 1.js
@@ -0,0 +1,8 @@
+// made 31/8/24
+// by francis igoe
+// does some basic math and prints the answers to the console
+
+console.log( -5 + 8 * 6);
+console.log((55+9) % 9 );
+console.log(20 + -3*5 / 8 );
+console.log(5 + 15 / 3 * 2 - 8 % 3);
---
+++ basics/Activity 2.js
@@ -0,0 +1,6 @@
+// made 31/8/24
+// by francis igeo 
+// takes two inputs from the user, devides them and shows the result
+let numerator = prompt("enter the dInput the first number:")
+let denominator = prompt("Input the second number:")
+console.log("The division of the first number and the second number is:", numerator / denominator)
